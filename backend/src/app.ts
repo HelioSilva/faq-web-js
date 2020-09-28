0d53c2b78c278fe1d9b036f68f0d1b68b8b1e171
Helio Silva <suporte.computek@gmail.com>
2020-09-28 01:01:04 -0300

    Alter typeorm

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,13 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 import cors from "cors";
 import routes from "./routes";
+
+import { createConnection, getConnectionOptions } from "typeorm";
+
+(async () => {
+  const defaultOptions = await getConnectionOptions();
+  await createConnection(defaultOptions);
+})();
 
 const app = express();
 app.use(cors());
