b7291e9038c98eef56ee359f6eefef26942c5f1a
Ronald Santos <ronaldcontact2019@gmail.com>
2020-09-30 14:21:47 -0300

    Begin components

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import Ronald from "../../components/qtdAcessos/index";
+import BotaoPadrao from "../../components/buttons/index";
 
-const Home: React.FC = () => <h1>Home</h1>;
+const Home: React.FC = () => (
+  <>
+    <h1>FaqWeb</h1>
+    <Ronald />
+
+    <h1>exemplo botão</h1>
+    {/* <BotaoPadrao props /> */}
+  </>
+);
 
 export default Home;
