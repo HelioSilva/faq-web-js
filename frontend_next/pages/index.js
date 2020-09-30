22445eb83f478a2b3a668a970a5e484b2f5e913e
Helio Silva <suporte.computek@gmail.com>
2020-09-30 09:40:38 -0300

    Install styled-components

--- frontend_next/pages/index.js
+++ frontend_next/pages/index.js
@@ -1,17 +1,25 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import styled from "styled-components";
 import Link from "next/link";
 import Teste from "./teste";
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
       <Head />
-      <h5>Hello (+) Helio</h5>
+      <Title>Home com aero function</Title>
       <p className={styles.title}>
         <Link href="/teste">TEs</Link>
       </p>
       <Teste type="erro">Helio Silva</Teste>
     </div>
   );
-}
+};
+
+const Title = styled.h1`
+  font-size: 55;
+  color: #2b6d8e;
+`;
+
+export default Home;
