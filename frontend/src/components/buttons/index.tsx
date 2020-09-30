671f99c6e62893d84e5e434274069223ed83afbe
Helio Silva <suporte.computek@gmail.com>
2020-09-30 16:21:49 -0300

    Styled in component Buttom

--- frontend/src/components/buttons/index.tsx
+++ frontend/src/components/buttons/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { Container } from "./style";
 
 const Button = ({ ...props }) => (
   <>
-    <p>{props.name}</p>
+    <Container>
+      <p>{props.name}</p>
+    </Container>
   </>
 );
 
--- frontend/src/components/buttons/style.ts
+++ frontend/src/components/buttons/style.ts
@@ -1,1 +1,25 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  cursor: pointer;
+  width: 200px;
+  height: 35px;
+  background-color: #d9462d;
+  border-radius: 5px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin: 5px;
+  transition-duration: 0.6;
+
+  &:hover {
+    background-color: #a6321e;
+  }
+
+  p {
+    font-size: 16px;
+    color: #fff;
+  }
+`;
