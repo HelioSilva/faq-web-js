48715a32d2a6d223f255edd6638b3dc3d5bc3966
Ronald Santos <ronaldcontact2019@gmail.com>
2020-09-30 14:26:23 -0300

    new components

---
+++ frontend/src/components/buttons/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Button = (nam9e: String) => (
+  <>
+    <p>${nam9e}</p>
+  </>
+);
+
+export default Button;
---
+++ frontend/src/components/qtdAcessos/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const QtdAcessos = () => (
+  <>
+    <p>helio</p>
+    <p>helio</p>
+  </>
+);
+
+export default QtdAcessos;
