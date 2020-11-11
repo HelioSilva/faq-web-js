e32ed5891c5d475caf586de0f90ca3c33856dcb7
Ronald Santos <ronaldcontact2019@gmail.com>
2020-11-11 15:59:30 -0300

    New version load annimation button

--- frontend/src/components/buttons/index.tsx
+++ frontend/src/components/buttons/index.tsx
@@ -1,19 +1,14 @@
 import styled from "styled-components";
 
 const ButtonFaq = styled.button.attrs({ href: "http://debian.org" })`
+  display:flex;
+  position:relative;
+  overflow: hidden;
+  
   height: 55px;
   width: 214px;
-  /* left: 1007px;
-  top: 363px; */
   border-radius: 27px;
   background: #c20b2e;
-
-  /* margin: 10px 0;
-  width: 214px;
-  height: 35px;
-  border-radius: 10px;
-  background: #c20b2e;
-  color: #fff; */
 
   display: flex;
   justify-content: center;
@@ -25,6 +20,29 @@ const ButtonFaq = styled.button.attrs({ href: "http://debian.org" })`
     background: #9d0926;
     color: #fff;
   }
+
+
+  &::before {
+    content: '';
+    position: absolute;
+    opacity: 1;
+    width: 100%;
+    height:100%;
+    background-color: blue;
+    transform-origin: bottom left;
+    transform: translateX(100%) scale(1.5) skew(-30deg);
+    transition: transform 200ms linear;
+  }
+  &::after{
+    content: "${(props) => props.children}";
+    position: absolute;
+  }
+  
+  &:hover::before{
+    transform: translateX(-30%) scale(1.5) skew(-30deg);
+  }
+
+
 `;
 
 export default ButtonFaq;
