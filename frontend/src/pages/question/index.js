060047dcd328837ecc7ba1ea29ac0759f3754f87
Helio Silva <suporte.computek@gmail.com>
2020-11-27 14:58:03 -0300

    Entendendo o funcionamento de formularios e envio via post

--- frontend/src/pages/question/index.js
+++ frontend/src/pages/question/index.js
@@ -1,23 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../../components/buttons/index_old";
 import Topo from "../../components/Topo";
-
+import api from "../../Services/api";
 
 function Question() {
-  return(
+  const [titulo, setTitulo] = useState("default");
+  const [autor, setAutor] = useState("");
+  const [acessos, setAcessos] = useState(0);
+
+  async function enviarQuestion(e) {
+    e.preventDefault();
+    console.log("pegando dados da api...");
+    const resposta = await api.post("/questions", {
+      titulo: titulo,
+      autor: autor,
+      autor_id: "e65daf66-dba2-4bb7-b22f-c836d193971",
+      acessos: acessos,
+    });
+
+    console.log(resposta);
+  }
+
+  return (
     <>
       <Topo />
-      <Button 
-      cor="red" 
-      name = "Editar"
-      />
-      <Button 
-      cor="blue" 
-      name = "Remover"
-      />
-    </>
-  )
+      <Button cor="red" name="Editar" />
+      <Button cor="blue" name="Remover" />
 
-};
+      <form onSubmit={enviarQuestion}>
+        <label>
+          Nome:
+          <input
+            type="text"
+            value={titulo}
+            onChange={(e) => {
+              setTitulo(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            value={autor}
+            onChange={(e) => {
+              setAutor(e.target.value);
+            }}
+          />
+          <input
+            type="number"
+            value={acessos}
+            onChange={(e) => {
+              setAcessos(e.target.value);
+            }}
+          />
+        </label>
+        <input type="submit" value="Enviar" />
+      </form>
+    </>
+  );
+}
 
 export default Question;
