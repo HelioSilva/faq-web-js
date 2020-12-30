959b61b1a938f71f79a0cb68f301167509beee4c
Helio Silva <suporte.computek@gmail.com>
2020-12-29 22:18:41 -0300

    Change limit backend json request to 128mb

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,11 @@ import { createConnection, getConnectionOptions } from "typeorm";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(
+  express.json({
+    limit: "128mb",
+  })
+);
 app.use(routes);
 app.use("/uploads", express.static("uploads"));
 
