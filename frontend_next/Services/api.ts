82b5ea94ee7bddd85e38470e824bcd7559c49d9c
Helio Silva <tsi.heliosilva@gmail.com>
2021-08-19 10:07:03 -0300

    CORS

--- frontend_next/Services/api.ts
+++ frontend_next/Services/api.ts
@@ -2,7 +2,15 @@ import axios from "axios";
 
 const api = axios.create({
   //baseURL: "http://3.91.230.251:3333",
-  baseURL: "http://107.23.251.120:3333",
+  baseURL: "http://faqcomputek.tk",
+  headers: {
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Origin": "https://www.faqcomputek.tk",
+    "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+  },
 });
+
+// api.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
+// api.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 
 export default api;
