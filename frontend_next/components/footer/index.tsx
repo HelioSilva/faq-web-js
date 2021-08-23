8343c4a55f33b8a39a5fdc0cdb3aad361cc47bb7
Helio Silva <tsi.heliosilva@gmail.com>
2021-08-23 17:11:03 -0300

    Migration MaterialUI

--- frontend_next/components/footer/index.tsx
+++ frontend_next/components/footer/index.tsx
@@ -1,31 +1,22 @@
-import Container from "../_systemUI/container";
-import GridContainer from "../_systemUI/gridContainer";
+// import Container from "../_systemUI/container";
+// import GridContainer from "../_systemUI/gridContainer";
+
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
 
 const Footer = () => {
   return (
-    <GridContainer
-      col={3}
-      spacing={2}
-      style={{
-        height: "50vh",
-        padding: "15px 10px 10px 10px",
-        background: "#864141",
-      }}
+    <Grid
+      container
+      justifyContent="space-between"
+      alignItems="center"
+      style={{ background: "#ad3c3c", height: "10vh" }}
     >
-      {/* <Container flex col flexCenter>
-        <h3>Desenvolvedores</h3>
-        <p>Helio Silva</p>
-        <p>Ronald Santos</p>
-      </Container>
-      <Container flex col flexCenter>
-        <h3>Projeto Open Source</h3>
-        <p>Projeto com código livre no Github</p>
-      </Container>
-      <Container flex col flexCenter>
-        <h3>Redes sociais</h3>
-        <p>Linkedin, Github, Instagran e outros</p>
-      </Container> */}
-    </GridContainer>
+      <Grid item xs />
+      <Grid item xs></Grid>
+      <Grid item xs></Grid>
+      <Grid item xs />
+    </Grid>
   );
 };
 
